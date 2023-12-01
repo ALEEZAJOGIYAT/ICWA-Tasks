88c7869eae8aa8ae3e2c2b2ee609ec78171b3f9c
Aleeza Jogiyat <aleeza.jogiyat@savyour.com>
2023-12-01 18:28:44 +0500

    updated

--- task1.sh
+++ task1.sh
@@ -7,14 +7,25 @@ source .env
 
 USERNAME="ALEEZAJOGIYAT"
 TOKEN=$token
-mkdir tasks
-cd tasks
-echo "migrating into the tasks folder"
-touch task1.py
-echo "Completing task1 using automation, It's soo much fun" > task1.py
 
+# Check if a commit message was provided as an argument, otherwise ask for input
+if [ -z "$1" ]; then
+    read -p "Enter commit message: " COMMIT_MSG
+else
+    COMMIT_MSG="$*"
+fi
+
+
+#creating a directory of tasks
+mkdir tasks
+cd tasks || exit
+#created a file within that directory
+touch task1.py
+echo "Compeleting task1 using automation, It's soo much fun" > task1.py
+
+#now pushing data into the github repository
 git add .
-git commit -m "pushing file using bash script"
+git commit -m "$COMMIT_MSG"
 
 git config credential.helper store
 
